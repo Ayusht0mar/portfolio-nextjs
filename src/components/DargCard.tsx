c45e081bb9105455057370ea109fe747f1b6c2e9
Ayush tomar <ayushtomar388@gmail.com>
2024-09-07 22:17:51 +0530

    Images Updated

--- src/components/DargCard.tsx
+++ src/components/DargCard.tsx
@@ -32,57 +32,61 @@ const Cards: React.FC = () => {
     <div className="absolute inset-0 z-10" ref={containerRef}>
       <Card
         containerRef={containerRef}
-        src="https://images.unsplash.com/photo-1635373670332-43ea883bb081?q=80&w=2781&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src="/emojis/brian-chesky.png"
         alt="Example image"
-        rotate="6deg"
-        top="20%"
-        left="25%"
-        className="w-36 md:w-56"
+        // rotate="6deg"
+        rotate="-18deg"
+        top="30%"
+        left="15%"
+        className="w-40 md:w-64"
+        // top="20%"
+        // left="25%"
+        // className="w-36 md:w-56"
       />
-      <Card
+      {/* <Card
         containerRef={containerRef}
-        src="https://images.unsplash.com/photo-1576174464184-fb78fe882bfd?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src="/emojis/captian-america.png"
         alt="Example image"
         rotate="12deg"
         top="45%"
-        left="60%"
+        left="30%"
         className="w-24 md:w-48"
-      />
+      /> */}
       <Card
         containerRef={containerRef}
-        src="https://images.unsplash.com/photo-1503751071777-d2918b21bbd9?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src="/emojis/elon-musk.png"
         alt="Example image"
         rotate="-6deg"
         top="20%"
-        left="40%"
+        left="30%"
         className="w-52 md:w-80"
       />
-      <Card
+      {/* <Card
         containerRef={containerRef}
-        src="https://images.unsplash.com/photo-1620428268482-cf1851a36764?q=80&w=2609&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src="/emojis/iron-man.png"
         alt="Example image"
         rotate="8deg"
         top="50%"
         left="40%"
         className="w-48 md:w-72"
+      /> */}
+      <Card
+        containerRef={containerRef}
+        src="/emojis/mark-zuckerberg.png"
+        alt="Example image"
+        rotate="6deg"
+        top="20%"
+        left="50%"
+        className="w-53 md:w-80"
       />
       <Card
         containerRef={containerRef}
-        src="https://images.unsplash.com/photo-1602212096437-d0af1ce0553e?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src="/emojis/steve-jobs.png"
         alt="Example image"
         rotate="18deg"
-        top="20%"
-        left="65%"
+        top="30%"
+        left="70%"
         className="w-40 md:w-64"
-      />
-      <Card
-        containerRef={containerRef}
-        src="https://images.unsplash.com/photo-1622313762347-3c09fe5f2719?q=80&w=2640&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="Example image"
-        rotate="-3deg"
-        top="35%"
-        left="55%"
-        className="w-24 md:w-48"
       />
     </div>
   );
@@ -127,7 +131,7 @@ export const Card: React.FC<CardProps> = ({
         zIndex,
       }}
       className={twMerge(
-        "drag-elements absolute w-48 bg-neutral-200 p-1 pb-4",
+        "drag-elements absolute w-48 border-8 border-b-[16px] border-neutral-200 bg-white",
         className
       )}
       src={src}
