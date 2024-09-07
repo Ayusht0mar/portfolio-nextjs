b90ae22cb6f903810e332efa43993b4570f88fbe
Ayush tomar <ayushtomar388@gmail.com>
2024-09-07 22:18:43 +0530

    Details Updated

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -17,7 +17,7 @@ const Experience = () => {
             />
             <Card className="p-6 overflow-hidden">
                 <Tabs defaultValue="Satyukt" className="flex flex-col md:flex-row gap-6">
-                    <TabsList className="flex justify-start md:flex-col overflow-y-scroll md:overflow-y-clip">
+                    <TabsList className="no-scrollbar flex justify-start md:flex-col overflow-y-scroll md:overflow-y-clip">
                         <TabsTrigger value="Satyukt" className="bg-white w-full text-base">Satyukt</TabsTrigger>
                         <TabsTrigger value="Storyflow" className="bg-white w-full text-base">Storyflow</TabsTrigger>
                         <TabsTrigger value="Digisell" className="bg-white w-full text-base">Digisell</TabsTrigger>
@@ -28,13 +28,13 @@ const Experience = () => {
                                 Web Developement Intern
                             </h2>
                             <p className="text-sm text-muted-foreground">
-                                July 2024 - Present
+                                July 2024 - August 2024
                             </p>
                             <p className="text-sm text-muted-foreground mb-2">
                                 Bengaluru, India
                             </p>
-                            <p>
-                                Worked as a web Developer
+                            <p className="max-w-xl">
+                                During my internship at Satyukt, I collaborate with the development team to design and implement responsive websites using HTML, CSS, and JavaScript. I contributed to the front-end enhancing user interfaces and optimizing website performance.
                             </p>
                         </TabsContent>
                         <TabsContent value="Storyflow">
@@ -47,8 +47,8 @@ const Experience = () => {
                             <p className="text-sm text-muted-foreground mb-2">
                                 Tilburg, Netherlands
                             </p>
-                            <p>
-                                Worked as a web Developer
+                            <p className="max-w-xl">
+                                During my internship at Storyflow, I collaborated with the founding team to design and implement responsive platforms using NextJs, NextAuth, and GraphQL. I contributed to both front-end and back-end development. My work included developing and maintaining web applications.
                             </p>
                         </TabsContent>
                         <TabsContent value="Digisell">
@@ -61,8 +61,8 @@ const Experience = () => {
                             <p className="text-sm text-muted-foreground mb-2">
                                 London, United Kingdom
                             </p>
-                            <p>
-                                Worked as a web Developer
+                            <p className="max-w-xl">
+                                As a freelance web developer, I successfully completed a project for Digisell, where I was responsible for designing and building a custom website template to allow users to create custom websites .
                             </p>                        
                         </TabsContent>
                         <TabsContent value="ShreeSarang">
@@ -73,10 +73,10 @@ const Experience = () => {
                                 March 2024
                             </p>
                             <p className="text-sm text-muted-foreground mb-2">
-                                London, United Kingdom
+                                Delhi, India
                             </p>
-                            <p>
-                                Worked as a web Developer
+                            <p className="max-w-xl">
+                                As a freelance web developer, I successfully completed a project for Shree Sarang, where I was responsible for designing and building an ecommerce website for the jewelry brand .
                             </p>
                         </TabsContent>
                 </Tabs>
