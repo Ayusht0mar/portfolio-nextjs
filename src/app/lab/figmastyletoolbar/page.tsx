0da055519e6da4d3103d874e2c37b3d92ab9d831
Ayush tomar <ayushtomar388@gmail.com>
2024-11-05 23:15:05 +0530

    Page Created

---
+++ src/app/lab/figmastyletoolbar/page.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa6";
+import CodeViewer from "@/components/lab/CodeViewer";
+import FigmaStyleToolbar from "../../../../public/lab/figmastyletoolbar/page";
+
+
+const Popover = () => {
+    return ( 
+        <div className="min-h-screen w-[96vw] lg:w-[60vw] mb-20 flex flex-col gap-4 mx-auto">
+            <Link 
+            href="/lab"
+            className="flex items-center gap-2"
+            >
+                <FaArrowLeft />
+                Back to lab
+            </Link>
+            <h2 className="font-semibold pl-2">Figma Style Toolbar</h2>
+            <div className="border bg-white rounded-lg overflow-hidden min-h-80">
+                <FigmaStyleToolbar/>
+            </div>
+            <div className="bg-[#1a1a1a] rounded-lg">
+                <CodeViewer filePath="lab/figmastyletoolbar/page.tsx"/>
+            </div>
+        </div>
+     );
+}
+ 
+export default Popover;
