83bbca5a1890cf70e9a5bd762d773aee9b069e3c
Ayush tomar <ayushtomar388@gmail.com>
2024-11-05 23:21:17 +0530

    Path Updated

--- src/app/lab/figmastyletoolbar/page.tsx
+++ src/app/lab/figmastyletoolbar/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { FaArrowLeft } from "react-icons/fa6";
 import CodeViewer from "@/components/lab/CodeViewer";
-import FigmaStyleToolbar from "../../../../public/lab/figmastyletoolbar/page";
+import FigmaStyleToolbar from "@/components/lab/FigmaStyleToolbar";
 
 
 const Popover = () => {
